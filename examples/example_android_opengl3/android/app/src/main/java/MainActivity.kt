22e4b7ba217a493068732eb6385fe968bc48e4c0
tuxltuxl001 <tuxltuxl001@gmail.com>
2024-12-23 17:26:59 +0000

    Build

--- examples/example_android_opengl3/android/app/src/main/java/MainActivity.kt
+++ examples/example_android_opengl3/android/app/src/main/java/MainActivity.kt
@@ -7,34 +7,44 @@ import android.view.inputmethod.InputMethodManager
 import android.view.KeyEvent
 import java.util.concurrent.LinkedBlockingQueue
 
+/**
+ * MainActivity class for the Android example.
+ */
 class MainActivity : NativeActivity() {
-    public override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val unicodeCharacterQueue: LinkedBlockingQueue<Int> = LinkedBlockingQueue()
 
+    /**
+     * Shows the soft input keyboard.
+     */
     fun showSoftInput() {
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.showSoftInput(this.window.decorView, 0)
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+        .showSoftInput(window.decorView, 0)
     }
 
+    /**
+     * Hides the soft input keyboard.
+     */
     fun hideSoftInput() {
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(this.window.decorView.windowToken, 0)
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+        .hideSoftInputFromWindow(window.decorView.windowToken, 0)
     }
 
-    // Queue for the Unicode characters to be polled from native code (via pollUnicodeChar())
-    private var unicodeCharacterQueue: LinkedBlockingQueue<Int> = LinkedBlockingQueue()
+    /**
+     * Polls the next Unicode character from the queue.
+     * @return The next Unicode character, or 0 if the queue is empty.
+     */
+    fun pollUnicodeChar() = unicodeCharacterQueue.poll() ?: 0
 
-    // We assume dispatchKeyEvent() of the NativeActivity is actually called for every
-    // KeyEvent and not consumed by any View before it reaches here
-    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+    /**
+     * Dispatches a key event.
+     * @param event The key event to dispatch.
+     * @return True if the event was handled, false otherwise.
+     */
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean =
         if (event.action == KeyEvent.ACTION_DOWN) {
             unicodeCharacterQueue.offer(event.getUnicodeChar(event.metaState))
-        }
-        return super.dispatchKeyEvent(event)
-    }
-
-    fun pollUnicodeChar(): Int {
-        return unicodeCharacterQueue.poll() ?: 0
+            super.dispatchKeyEvent(event)
+        } else {
+            super.dispatchKeyEvent(event)
     }
 }
